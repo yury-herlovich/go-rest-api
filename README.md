a656ecce66ab35a5164e410facf58672cb43555e
Yury Herlovich <yuryherlovich@gmail.com>
2022-04-08 18:00:06 -0700

    add postgres container

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 - Go 1.17+
 - Reflex - https://github.com/cespare/reflex
 - VSCode extension Rest Client or similar to make requests from api.http text file
+
+### Install
+- create `.env` file - `cp .env-sample .env` and set values if necessary
--- src/main.go
+++ src/main.go
@@ -1,11 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+
 	"github.com/gin-gonic/gin"
 	"github.com/yury-herlovich/go-rest-api/src/albums"
 )
 
+const (
+	host     = "db"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "gorestdb"
+)
+
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("DB successfully connected!")
+	}
+
 	r := gin.Default()
 	r.GET("/albums", albums.GetAlbums)
 	r.POST("/albums", albums.AddAlbum)
